656b2c1f4dbeaa84146ef0d5cb95d599315d71af
Pablo Osinaga <paguos@gmail.com>
2021-09-24 16:50:02 +0200

    fix: berlin range scenario config

---
+++ app/nes-applications/src/main/java/com/github/paguos/mosaic/app/config/CSpeedSensorApp.java
@@ -0,0 +1,11 @@
+package com.github.paguos.mosaic.app.config;
+
+public class CSpeedSensorApp {
+
+    /**
+     * Send the messages to road side units.
+     * Otherwise send them directly to the moving sink.
+     */
+    public boolean rsuEnabled = true;
+
+}
