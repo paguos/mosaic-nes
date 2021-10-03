2f9847c8c17b79704b7a20605c8c5cf37dfb0198
Pablo Osinaga <paguos@gmail.com>
2021-10-03 12:43:42 +0200

    refactor(tools): vehicle analysis tool

--- tools/experiments-analyzer/vehicle.py
+++ tools/experiments-analyzer/vehicle.py
@@ -1,26 +1,32 @@
 import pandas as pd
 
-scenario = "barcelona"
+from loguru import logger
+
+scenario = "leipzig"
 experiment_name = "variable_source_interval"
-interval = 100
+interval = 500
+
+logger.info("Starting vehicle analyzis ...")
 
 baseline_results = f"/home/parallels/Documents/experiments/{scenario}/{experiment_name}/baseline.csv"
 expermient_results = f"/home/parallels/Documents/experiments/{scenario}/{experiment_name}/interval_{interval}.csv"
 
 field_names = ["vehicleId", "timestamp", "latitude", "longitude", "speed"]
-vehicle = "veh_5"
 
+logger.info("Reading datasets ...")
 df_base = pd.read_csv(baseline_results, names=field_names)
 df_exp = pd.read_csv(expermient_results, names=field_names)
+logger.info("Reading datasets ... done!")
 
-baseline_len = df_base.shape[0]
-exp_len = df_exp.shape[0]
+for i in range(1,8):
+    vehicle = f"veh_{i}"
+    logger.deug(f"Generating dataset of vehicle '{vehicle}' ...")
 
-veh_base = df_base[df_base["vehicleId"]==vehicle].reset_index(drop=True)
-veh_exp = df_exp[df_exp["vehicleId"]==vehicle].reset_index(drop=True)
+    veh_base = df_base[df_base["vehicleId"]==vehicle].reset_index(drop=True)
+    veh_exp = df_exp[df_exp["vehicleId"]==vehicle].reset_index(drop=True)
 
-print(veh_base.shape)
-print(veh_exp.shape)
+    veh_base.to_csv(f"baseline_{vehicle}.csv", sep=',', index=False)
+    veh_exp.to_csv(f"interval_{interval}_{vehicle}.csv", sep=',', index=False)
+    logger.debug(f"Generating dataset of vehicle '{vehicle}' ... done!")
 
-veh_base.to_csv(f"baseline_{vehicle}.csv", sep=',', index=False)
-veh_exp.to_csv(f"interval_{interval}_{vehicle}.csv", sep=',', index=False)
+logger.info("Vehicle analyzis completed!")
