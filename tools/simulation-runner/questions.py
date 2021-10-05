5e21beddf3c1e1b335168f833a1c691a8e38262d
Pablo Osinaga <paguos@gmail.com>
2021-10-05 19:06:33 +0200

    feat(sim-runner): add experiment configuration

---
+++ tools/simulation-runner/questions.py
@@ -0,0 +1,57 @@
+import os
+
+from pathlib import Path
+
+questions = [
+        {
+            'type': 'list',
+            'name': 'experiment',
+            'message': 'Experiment name:',
+            'choices': [
+                'variable_update_intervals',
+                'variable_coordinator_update_interval',
+                'variable_source_update_interval',
+                'max_speed',
+                'max_speed_sink',
+                'max_speed_source',
+                'moving_range',
+                'rsu_range'
+            ],
+        },
+        {
+            'type': 'list',
+            'name': 'scenario',
+            'message': 'Scenario:',
+            'choices': ['barcelona', 'barcelona-moving-range', 'barcelona-range-query', 'berlin', 'berlin-moving-range', 'berlin-range-query'],
+        },
+        {
+            'type': 'input',
+            'name': 'brake',
+            'message': 'Brake:',
+            'default': '1'
+        },
+        {
+            'type': 'input',
+            'name': 'config_start',
+            'message': 'Start value:',
+            'default': '0'
+        },
+        {
+            'type': 'input',
+            'name': 'config_interval',
+            'message': 'Interval value:',
+            'default': '500'
+        },
+        {
+            'type': 'input',
+            'name': 'config_repetitions',
+            'message': 'Number of repetitions:',
+            'default': '5'
+        },
+        {
+            'type': 'input',
+            'name': 'mosaic_path',
+            'message': 'Mosaic path:',
+            'default': f"{Path(os.getcwd()).parent.parent}/mosaic"
+        }
+    ]
--- tools/simulation-runner/runner.py
+++ tools/simulation-runner/runner.py
@@ -5,16 +5,18 @@ import os
 from loguru import logger
 from pathlib import Path
 from PyInquirer import prompt
+from experiment import ExperimentFactory
 
+from questions import questions
 from simulation import Configuration, Simulation
 
 class SimulationRunner:
 
-    def __init__(self, name: str, scenario: str, configs: list, mosaic_path: Path):
+    def __init__(self, name: str, scenario: str, experiments: list, mosaic_path: Path):
         self.cwd = Path(f"{os.getcwd()}/experiments")
         self.name = name
         self.scenario = scenario
-        self.configs = configs
+        self.experiments = experiments
         self.mosaic_path = mosaic_path
 
     def start(self, brake=1):
@@ -23,90 +25,30 @@ class SimulationRunner:
             self.cwd.mkdir(parents=True)
         os.chdir(self.mosaic_path)
 
-        for config in self.configs:
-            logger.info(f"Starting {self.scenario} with config {config.attribute} -> {config.value} ...")
-            config.apply()
-            simulation = Simulation(self.scenario, f"{self.name}_{config.attribute}_{config.value}", brake)
+        for experiment in self.experiments:
+            logger.info(f"Starting {self.scenario} for experiment {experiment.name} with value {experiment.value} ...")
+            experiment.apply()
+            simulation = Simulation(self.scenario, f"{self.name}_{experiment.value}", brake)
             simulation.run()
             simulation.copy_results(self.cwd)
-            logger.info(f"{self.scenario} with config {config.attribute} -> {config.value} done!")
+            experiment.reset()
+            logger.info(f"Starting {self.scenario} for experiment {experiment.name} with value {experiment.value} ... done!")
         
         logger.info("Simulation runner done!")
 
 if __name__ == "__main__":
-
-    questions = [
-        {
-            'type': 'input',
-            'name': 'experiment',
-            'message': 'Experiment name:'
-        },
-        {
-            'type': 'list',
-            'name': 'scenario',
-            'message': 'Scenario:',
-            'choices': ['barcelona', 'barcelona-moving-range', 'barcelona-range-query', 'berlin', 'berlin-moving-range', 'berlin-range-query'],
-        },
-        {
-            'type': 'input',
-            'name': 'brake',
-            'message': 'Brake:',
-            'default': '1'
-        },
-        {
-            'type': 'list',
-            'name': 'config_file',
-            'message': 'File for config:',
-            'choices': ['application/NesApp.json', 'mapping/mapping.json', 'nes/nes_config.json'],
-        },
-        {
-            'type': 'input',
-            'name': 'config_attribute',
-            'message': 'Attribute for config:',
-            'default': 'updateLocationInterval'
-        },
-        {
-            'type': 'input',
-            'name': 'config_start',
-            'message': 'Start value for config:',
-            'default': '0'
-        },
-        {
-            'type': 'input',
-            'name': 'config_interval',
-            'message': 'Interval value for config:',
-            'default': '500'
-        },
-        {
-            'type': 'input',
-            'name': 'config_repetitions',
-            'message': 'Number of repetitions for config:',
-            'default': '5'
-        },
-        {
-            'type': 'input',
-            'name': 'mosaic_path',
-            'message': 'Mosaic path:',
-            'default': f"{Path(os.getcwd()).parent.parent}/mosaic"
-        }
-    ]
-
     answers = prompt(questions)
     scenario = answers.get("scenario")
-
-    configs = []
-    config_attribute = answers.get("config_attribute")
-
+    experiments = []
     for i in range(0, int(answers.get("config_repetitions"))):
         value = int(answers.get("config_start")) + (i * int(answers.get("config_interval")))
-        config_file = Path(f"scenarios/{scenario}/{answers.get('config_file')}")
-        config = Configuration(config_file, config_attribute, value)
-        configs.append(config)
+        experiment = ExperimentFactory.create(scenario, answers.get("experiment"), value)
+        experiments.append(experiment)
 
-    experiment_name = answers.get('experiment')
+    experiment = answers.get('experiment')
     mosaic_path = Path(answers.get('mosaic_path'))
     brake_value = int(answers.get('brake'))
 
-    runner = SimulationRunner(experiment_name, scenario, configs, mosaic_path)
+    runner = SimulationRunner(experiment, scenario, experiments, mosaic_path)
     runner.start(brake=brake_value)
     
